59f543331524ef9cf556a0b76b447cee6fc55850
Matheus Guimaraes Castiglioni Gorza <matguima@c2r3s6.42.rio>
2025-11-20 20:55:34 -0300

    new commit

--- campo-minado.c
+++ campo-minado.c
@@ -11,7 +11,7 @@ neighbor: 0 a 4
 typedef struct
 {
 	int isBomb;
-	int	open;
+	int	isOpen;
 	int	neighbor;
 }	cell;
 //variáveis globais
@@ -22,13 +22,13 @@ int	l, c, size = 10;
 //função para iniciar a matriz do jogo. Limpa toda matriz.
 void play_game()
 {
-	for(l = 0; l < tam; l++)
+	for(l = 0; l < size; l++)
 	{
-		for(c = 0; c < tam; c++)
+		for(c = 0; c < size; c++)
 		{
 			game[l][c].isBomb = 0;
-			game[l][c].open = 0;
-			game[l][c].neigjbor = 0;
+			game[l][c].isOpen = 0;
+			game[l][c].neighbor = 0;
 		}
 	}
 }
@@ -38,8 +38,8 @@ void new_bombs(int n)
 	srand(time(NULL));
 	for(int i = 0; i <= n; i++)
 	{
-		l = rand() % tam;
-		c = rand() % tam;
+		l = rand() % size;
+		c = rand() % size;
 		if (game[l][c].isBomb == 0)
 			game[l][c].isBomb = 1;
 		else
@@ -47,11 +47,46 @@ void new_bombs(int n)
 	}
 }
 
+//função que diz se um par de cordenadas é false ou true (0 ou 1)
+int	is_valid(int l, int c)
+{
+	if((l >= 0 && l < size) && (c >= 0 && c < size))
+		return (1);
+	else
+		return (0);
+}
+
+//função que retorna quantidade de bombas
+int	neighbor_bombs(int l, int c)
+{
+	int count = 0;
+	if(is_valid(l-1, c) && game[l-1][c].isBomb)
+		count++;
+	if(is_valid(l+1, c) && game[l+1][c].isBomb)
+		count++;
+	if(is_valid(l, c-1) && game[l][c+1].isBomb)
+		count++;
+	if(is_valid(l, c+1) && game[l][c-1].isBomb)
+		count++;
+	return (count);
+}
+
+//função para contar as bombas vizinhas
+void count_bombs()
+{
+	for(l = 0; l < size; l++)
+	{
+		for(c = 0; c < size; c++)
+		{
+			game[l][c].neighbor = neighbor_bombs(l, c);
+		}
+	}
+}
 
 int main(void)
 {
 	play_game();
 	new_bombs(10);
-
+	count_bombs();
 	return (0);
 }
